f104252457b540ee0932dde11175665a3be15215
krauselidia <lidia240105@edu.unipar.br>
2026-04-10 20:22:38 -0300

    Initial commit

---
+++ saudepetclinic/src/main/java/com/petclinic/saudepetclinic/dto/CadastroDTO.java
@@ -0,0 +1,18 @@
+package com.petclinic.saudepetclinic.dto;
+
+public class CadastroDTO {
+
+    public String nome;
+    public String cpf;
+    public String telefone;
+    public String endereco;
+    public String numero;
+    public String cep;
+
+    public String nomeAnimal;
+    public String especie;
+    public String raca;
+    public Integer idade;
+    public String sexo;
+    public Double peso;
+}
---
+++ saudepetclinic/src/test/java/com/petclinic/saudepetclinic/SaudepetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.saudepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudepetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
